601a06fd56843b62a88af1e505ce70977839e630
Vzero0zero <vzero000@protonmail.com>
2024-08-16 20:11:11 -0300

    Add files via upload

---
+++ codeLivro.cpp
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <img src="imagens/aLendaDoVento.jpg" height=400>
+        <title>A Lenda do vento</title>
+    </head>
+<Body>
+    <a>Parte 1</a>
+    <h1>   Ventos que Levam
+    </h1>
+    <p>
+    
+        
+        
+        Buscava de forma sutil seus próximos passos Damarunia era um viajante de carta cheia, em uma moto de passeio clássica levava adiante seu legado, uma busca concisa que lhe pertencia a duas décadas era normal encontrar-se em locais longínquos aos quais desfrutava de uma contemplação com a paisagem, era m local quente de vários desfiladeiros estradas planas desérticas, sua vestimenta rustica o ajudava a prevalecer em frente, seus cabelos quase grisalhos para cima , um homem pardo a meia altura levava consigo um para de roupas sempre que as necessitava, alojou sua moto em um hotel por um mês e tomou o caminho para uma profunda busca pessoal. Levava em sua bolsa o suficiente para sobreviver, Além da faca de caça, sabia os mapas daquele local quase decór, após dois dias de muita caminhada, alojou em meditação. Relembrou os preceitos de sues dons de conversar com os espíritos, o amuleto que seu pai um mestiço xamã lhe presenteara quando adolescente lhe brotou caminhos alternativos aos usuais, Ali um espirito em meio a fogueira lhe disse que ele tomaria em si algo naquele momento que mudaria sua vida, o amuleto começou a brilhar era como um talismã ungido em ouro Branco como um selo rúnico, dali uma luz prateada brotou de seu todo e cresceu sobre si, quando ele o deixou sobre o chão, a luz rajada aumentou sobre a queda alaranjada da noite que pairava como um rastro fantasmagólico uma águia de dois metros saiu em meio a luz com a mesma tonalidade prateada deu uma volta em torno de dez metros naquele campo enquanto Damarunia olhava fixamente, como um magnetismo o deixou catatônico veio em sua direção e tomou seu corpo por completo sentiu- se leve estava flutuando meio metro acima do chão. Não há mais tempo a perder o espirito falou-lhe. Reservei minhas ultimas energias para esse momento, saberá muito bem oque fazer, você foi guiado para isso, sua nova jornada começa agora aventureiro. Enquanto ele descia sobre o chão o espirito do índio que lhe falava sumia sutilmente enquanto uma torrente de energia tomava o lugar dele eram um magnestismo forte que distorcia aquele pedaço de espaço, o homem lhe estendeu a mão ele a tocou como um gesto de suporte, sentiu um calafrio imenso e uma luz forte clareou toda sua visão era como estar dentro de um vendaval tremendo, não conseguia explicar se viu em estado de flutuação. E lá estava caindo sobre as nuvens de um céu anuviado local desértico de uma única montanha de cerca de cem quilômetros a sua frente. Não era o mesmo local.
+        <p>
+        (voa até a montanha e não pousa no vendaval se instala nas beiradas)
+        </p>
+        <p>
+        A ventania ressoava ao plano do solo entre as vigas e palanques que se estabeleciam a sua frente. Tão incerto como tolo viajante que não conhece o caminho que busca e mesmo assim aprendeu a sobreviver, era claro que aquele que pisasse sobre o chão daquela quadra de obstáculos suntuosa seria espancado por pilares e postes que sopravam ensurdecedores, alcançar aquela altitude havia sido um dos maiores desafios que Damarunia encontrara em suas aventuras(detalhar a subida).
+        </p>
+        <p>
+        A busca da descoberta o incentivava ir além, pois oque haveria adiante destes confins? Por algum momento contestou suas ações que o levavam a tal desafio, mas acabara de sumonar grande parte do poder do amuleto que carregara por  tanto tempo sem descobrir tamanho refulgor em levitação.(descrição da cena do primeiro voo na queda).
+        </p>
+        <p>
+        O desafio a frente carrancas e totens a frente o estigavam ao mistério de suas criações. Hieróglifos e gravuras quase rupestres se estabeleciam sobre as pedras que mal se mexiam e como estas se mantinham firmes sobre a ventania descomunal era um segredo.
+        </p>
+        <p>
+        Seus cálculos definiam ventos cortantes de além dos duzentos quilómetros por hora. Visualizava com os olhos sobre o topo da montanha lá estava o desafio que buscava encontrar por tanto tempo.
+        </p>
+        <p>
+        Os restos daquilo que ali sobrasse corria sobre o abismo a esquerda rente e de lá abaixo, sobre fendas e alturas incalculáveis.
+        </p>
+        <p>
+        Concentrou sua mente e seu fluxo de energia vital sobre o amuleto já não podia perder tempo. Ali estava e era o omento de continuar ou sabe-se lá quando poderia estar diante de tamanha oportunidade sobre sua jornada.
+        </p>
+        <p>
+        Ele se focou por um momento enquanto observava o plano a sua frente, estava deitado sobre a montanha de pedras fixas as margens da montanha ao qual caiu dos céus.
+        </p>
+        <p>
+        Concentrou sua atenção sobre a totalidade de seu corpo, como se fizesse  cada parte dos ventos cortantes esquivarem do choque contra si enquanto saltava em um rodopio a frente com um mortal. Ao fixar-se sobre o chão mesmo com a ventania esquiva sentia- se levemente escorregadio e levado alguns centimetros sentido ao sopro do vento. Percebeu que mesmo com seu foco sobre a magia especifica emanava sua energia estava sendo drenada e não conseguiria manter por muito tempo, fixou a telecinesia sobre os pés como -se cada passo pesasse dez quilos além do normal, seguiu em frente adiante os obstáculos enquanto memorizava claramente cada imagem gravada sobre as rochas detalhadas que remetiam arenito. Era preciso move-las segundo seu discernimento. Após visualizar todo cenário além de muitos tributos e ritos, percebeu sobre seus seus dos desvendando mistérios e similaridades nos objetos que ali estavam entre algumas imagens que como uma espécie de culto a uma energia focavam em m centro especifico cerca de três pessoas de cada lado que lembrava a figura de índios, memorizou especificamente uma espécie de distribuição dos objetos que ali estavam em circulo.
+        </p>
+        <p>
+        Sua energia quase esgotada ao fim da investigação.
+        </p>
+        <p>
+        Era hora de voltar ao ponto de partida, após o reconhecimento da figura de centro sobre as lapides entre postes e blocões de uma espécie de arenito. Em um salto súbito o rodopio novamente o deslocou dos ventos fortes para as laterais sobressalientes da montanha.
+        </p>
+        <p>
+        Um momento de exaustão suntuoso comoveu seu temperamento e sua vontade de permanecer fixo sobre o local.
+        </p>
+        <p>
+        Uma luz radiante sobre as nuvens chamou sua atenção em uma concentração de ventos que vinham dos quatro cantos. Sentiu que o amuleto o atraia para a subida sobre os céus. Fechou os olhos segurando firmemente onde estava e saltou para traz deitando sobre o ar liberando o final da energia que lhe restava. Voou com uma subida em espiral sobre a abertura em espiral entre os feixes de luz solar. Quando sobressaiu em elevação as nuvens foi como se o amuleto se conectasse aos ventos com linhas efêmeras ressoantes douradas, flutuou por um momento vendo suas energias totais serem regeneradas. Olhou firmemente sobre as frestas do portal no céu e lá estava abaixo o desafio de mover aquelas estruturas de uma forma especifica, observou atenciosamente calculando os próximos movimentos, enquanto se revitalizava e mergulhou em um voo rasante.
+        </p>
+        <p>
+        Com o impulso da decida pressionou as correntes de ar sobre um pilar fixando seus pés sobre o chão com uma pressão amortecendo o voo. O vento moveu aquele pilar de uma forma mecânica sem deixar sinais de mecanismos abaixo de seu percurso, era sem dúvidas magia.
+        </p>
+        <p>
+        O vento permanecia voraz, foi assim que quase como um tufão dominou a direção dos ventos sobre si e aos arredores a direcionando novamente com saltos rotatórios laterais e apontando a palma da mão aberta fixou mais dois pilares sobre a imagem circular que memorizara na forma que traduzira. Concentrou mais uma vez o poder dos ventos em seus punhos sobre as laterais de sua cintura com os braços contidos mãos fechadas para cima, como um rugido expandido as mãos a frente liberando a energia sobre a lapide central, uma espécie de arca, fixou a sobre o centro. Com os ventos a seu favor correu sobre as laterais e chutou a ventania a ventania nos pilares que se moveram até o local que se propusera.
+        </p>
+        <p>
+        No momento em que tudo estava no lugar uma parada súbita no vento pairou e um estrondo seco começou a partir o centro do local em dois, algo ao centro que se abriu na cratera.
+        </p>
+        <p>
+        (Luz que guia o personagem para sua missão)
+        </p>
+    <p>
+Continua...
+    </p>
+</Body>
+    </html>
